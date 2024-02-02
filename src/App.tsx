137406c77120c756e4fbdf6d3fb59c77332de89c
Ura427 <urakost427@gmail.com>
2024-02-02 18:07:04 +0200

    modify app

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card, { CardVariant } from "./components/Card";
-import Card2, { Card2Variant } from "./components/Card2";
-import Card3, { Card3Variant } from "./components/Card3";
+
+import { ITodo } from "./types/types";
+import TodoList from "./components/TodoList";
+import AddTodo from "./components/AddTodo";
 
 function App() {
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = () => {
+    setTodos([
+      {
+        id: 1,
+        task: "Learn how to work with interfaces",
+        completed: true,
+        created: new Date(),
+      },
+      {
+        id: 2,
+        task: "Fix the fridge",
+        completed: false,
+        created: new Date(),
+      },
+    ]);
+  };
+
   return (
-    <div>
-      <Card
-        id={0}
-        text="Create project"
-        status={true}
-        variant={CardVariant.outlined}
-        onClick={() => console.log("Clicked!")}
-      >
-        Some text
-      </Card>
-      <Card
-        id={1}
-        text="Create ts component"
-        status={true}
-        variant={CardVariant.primary}
-      ></Card>
-      <Card
-        id={2}
-        text="Repeat creating ts component for 7 times"
-        status={true}
-        variant={CardVariant.outlined}
-      ></Card>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <div>
+        <AddTodo></AddTodo>
+        <TodoList todos={todos}></TodoList>
 
-      <Card2
-        id={10}
-        text="Create project"
-        status={true}
-        variant={Card2Variant.primary}
-        onClick={() => console.log("Clicked")}
-      >
-        somfpdsmpsmc
-      </Card2>
-      <Card2
-        id={11}
-        text="Create ts component"
-        status={true}
-        variant={Card2Variant.outlined}
-        onClick={() => console.log("Password")}
-      >
-        <input placeholder="Input password"></input>
-      </Card2>
-      <Card2
-        id={12}
-        text="Repeat creating ts component for 7 times"
-        status={true}
-        variant={Card2Variant.primary}
-        onClick={() => console.log("Table")}
-      >
-        <table></table>
-      </Card2>
-
-      <Card3
-        id={0}
-        text="Create project"
-        status={true}
-        variant={Card3Variant.outlined}
-      ></Card3>
-      <Card3
-        id={1}
-        text="Create ts component"
-        status={true}
-        variant={Card3Variant.primary}
-      ></Card3>
-      <Card3
-        id={2}
-        text="Repeat creating ts component for 7 times"
-        status={true}
-        variant={Card3Variant.outlined}
-      ></Card3>
+        <Card
+          id={0}
+          text="Create project"
+          status={true}
+          variant={CardVariant.outlined}
+          onClick={() => console.log("Clicked!")}
+        >
+          Some text
+        </Card>
+        <Card
+          id={1}
+          text="Create ts component"
+          status={true}
+          variant={CardVariant.primary}
+        ></Card>
+        <Card
+          id={2}
+          text="Repeat creating ts component for 7 times"
+          status={true}
+          variant={CardVariant.outlined}
+        ></Card>
+      </div>
     </div>
   );
 }
