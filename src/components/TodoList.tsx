42d3cc7ee33f9333a0753d218ddccf5828ec3f2c
Ura427 <urakost427@gmail.com>
2024-02-04 18:08:55 +0200

    prop drilling

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,14 +3,20 @@ import { ITodo } from "../types/types";
 import TodoItem from "./TodoItem";
 
 interface TodoListProps{
-    todos: ITodo[]
+    todos: ITodo[],
+    setTodos: React.Dispatch<React.SetStateAction<ITodo[]>>
 }
 
-const TodoList: React.FC<TodoListProps> = ({todos}) => {
+
+
+const TodoList: React.FC<TodoListProps> = ({todos, setTodos}) => {
+
+
+
   return (
     <div>
       {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
+        <TodoItem key={todo.id} todo={todo} todos={todos} setTodos={setTodos} />
       ))}
     </div>
   );
