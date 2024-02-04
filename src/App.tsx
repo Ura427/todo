306c1fdd790f9c2bf34d1261ad7ffba7cb7e2db3
Ura427 <urakost427@gmail.com>
2024-02-04 18:01:06 +0200

    rid of useEffect, rid of cards

--- src/App.tsx
+++ src/App.tsx
@@ -6,64 +6,36 @@ import TodoList from "./components/TodoList";
 import AddTodo from "./components/AddTodo";
 
 function App() {
-  const [todos, setTodos] = useState<ITodo[]>([]);
-
-  useEffect(() => {
-    fetchTodos();
-  }, []);
-
-  const fetchTodos = () => {
-    setTodos([
-      {
-        id: 1,
-        task: "Learn how to work with interfaces",
-        completed: true,
-        created: new Date(),
-      },
-      {
-        id: 2,
-        task: "Fix the fridge",
-        completed: false,
-        created: new Date(),
-      },
-    ]);
-  };
+  const [lastId, setLastId] = useState<number>();
+  const [todos, setTodos] = useState<ITodo[]>([
+    {
+      id: 1,
+      task: "Learn how to work with interfaces",
+      completed: false,
+      created: new Date().toDateString(),
+    },
+    {
+      id: 2,
+      task: "Fix the fridge",
+      completed: false,
+      created: new Date().toDateString(),
+    },
+  ]);
 
   return (
     <div
       style={{
         display: "flex",
         flexDirection: "column",
-        justifyContent: "center",
+        // justifyContent: "center",
         alignItems: "center",
         height: "100vh",
       }}
     >
-      <div>
-        <AddTodo></AddTodo>
-        <TodoList todos={todos}></TodoList>
-
-        <Card
-          id={0}
-          text="Create project"
-          status={true}
-          variant={CardVariant.outlined}
-          onClick={() => console.log("Clicked!")}
-        >
-          Some text
-        </Card>
-        <Card
-          id={1}
-          text="Create ts component"
-          status={true}
-          variant={CardVariant.primary}
-        ></Card>
-        <Card
-          id={2}
-          text="Repeat creating ts component for 7 times"
-          status={true}
-          variant={CardVariant.outlined}
-        ></Card>
+      <h1>Todo App</h1>
+      <AddTodo setTodos={setTodos}></AddTodo>
+      <div style={{ margin: "2rem" }}>
+        <TodoList todos={todos} setTodos={setTodos}></TodoList>
       </div>
     </div>
   );
