239bb0527a898dd557848d56c0f18f1e8e3639a7
Ura427 <urakost427@gmail.com>
2024-02-04 18:08:19 +0200

    create checkbox handler, change props

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -3,9 +3,20 @@ import { ITodo } from "../types/types";
 
 interface TodoItemProps {
   todo: ITodo;
+  todos: ITodo[],
+  setTodos: React.Dispatch<React.SetStateAction<ITodo[]>>
 }
 
-const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
+
+
+const TodoItem: React.FC<TodoItemProps> = ({ todo, todos, setTodos }) => {
+
+    // const 
+
+    const handleCheckboxChange = (id: number) => {
+        setTodos(todos.filter((todo: ITodo) => todo.id !== id))
+    }
+
   return (
     <div
       style={{
@@ -14,7 +25,8 @@ const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
         padding: "0.5rem",
       }}
     >
-      {todo.id}: {todo.task} <input type="checkbox" checked={todo.completed}/> {todo.created.toString()}
+      {todo.id}: {todo.task} <input type="checkbox" checked={todo.completed} onChange={() => handleCheckboxChange(todo.id)} />{" "}
+      {todo.created.toString()}
     </div>
   );
 };
