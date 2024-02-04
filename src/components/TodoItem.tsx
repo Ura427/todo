8ae6929086d28f0b2662685d67cb17f67f681a83
Ura427 <urakost427@gmail.com>
2024-02-04 21:12:51 +0200

    add possibility to edit todo

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { ITodo } from "../types/types";
+import EditIcon from "@mui/icons-material/Edit";
+import DoneIcon from "@mui/icons-material/Done";
 
 interface TodoItemProps {
   todo: ITodo;
-  todos: ITodo[],
-  setTodos: React.Dispatch<React.SetStateAction<ITodo[]>>
+  todos: ITodo[];
+  setTodos: React.Dispatch<React.SetStateAction<ITodo[]>>;
 }
 
-
-
 const TodoItem: React.FC<TodoItemProps> = ({ todo, todos, setTodos }) => {
+  const [disabled, setDisabled] = useState<boolean>(true);
+   const [task, setTask] = useState<string | undefined>(todo.task)
+    
+  const handleCheckboxChange = (id: number) => {
+    setTodos(todos.filter((todo: ITodo) => todo.id !== id));
+  };
 
-    // const 
+  const handleTaskChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTask(e.target.value)
+  }
 
-    const handleCheckboxChange = (id: number) => {
-        setTodos(todos.filter((todo: ITodo) => todo.id !== id))
-    }
+  const btnClickHandler = (id: number) => {
+    setDisabled((prev) => !prev);
 
+    // if (disabled === true) {
+    //   setTodos(
+    //     todos.map((todo) => {
+    //       if (todo.id === id) {
+    //         todo[task] ===
+    //       }
+    //     })
+    //   );
+    // }
+  };
   return (
     <div
       style={{
@@ -25,7 +42,25 @@ const TodoItem: React.FC<TodoItemProps> = ({ todo, todos, setTodos }) => {
         padding: "0.5rem",
       }}
     >
-      {todo.id}: {todo.task} <input type="checkbox" checked={todo.completed} onChange={() => handleCheckboxChange(todo.id)} />{" "}
+      {todo.id}:{" "}
+      {disabled === true ? <>{task}</> : <><input type="text"  value={task} onChange={handleTaskChange}></input>{" "}</> }
+      
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => handleCheckboxChange(todo.id)}
+      />
+      <button onClick={() => btnClickHandler(todo.id)}>
+        {disabled === true ? (
+          <>
+            <EditIcon /> Edit
+          </>
+        ) : (
+          <>
+            <DoneIcon></DoneIcon> Done
+          </>
+        )}
+      </button>{" "}
       {todo.created.toString()}
     </div>
   );
