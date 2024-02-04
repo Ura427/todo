dfd15c280c5db707638a8a33d80a7612e8322d20
Ura427 <urakost427@gmail.com>
2024-02-04 18:01:50 +0200

    create props interface, btn handler

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,15 +1,46 @@
 import React, { useState } from "react";
+import { ITodo } from "../types/types";
 
-function AddTodo() {
+interface AddTodoProps {
+  setTodos: React.Dispatch<React.SetStateAction<ITodo[]>>;
+}
+
+const AddTodo: React.FC<AddTodoProps> = ({ setTodos }) => {
   const [inputValue, setInputValue] = useState<string>();
+  const [lastId, setLastId] = useState<number>(3);
 
   const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   };
 
   const btnClickHandler = (e: React.MouseEvent) => {
-    console.log("BTN clicked")
-  }
+    // Create a new Date object from the date string
+    const date = new Date();
+
+    // Get the month, date, year, hours, minutes, and seconds from the Date object
+    const month = date.toLocaleString("en", { month: "short" });
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    // Format the date in the desired format
+    const formattedDate = `${month} ${day} ${year} ${hours}:${minutes}:${seconds}`;
+
+    //Create new Todo obj
+    const obj: ITodo = {
+      id: lastId,
+      task: inputValue,
+      completed: false,
+      created: formattedDate,
+    };
+
+    setTodos((prevState: ITodo[]) => [...prevState, obj]);
+    setInputValue("");
+    setLastId((prev) => prev + 1);
+    console.log("BTN clicked");
+  };
 
   return (
     <div>
@@ -17,6 +48,6 @@ function AddTodo() {
       <button onClick={btnClickHandler}>Add</button>
     </div>
   );
-}
+};
 
 export default AddTodo;
