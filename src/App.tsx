c1000ed4cd1049bd108b98dfefe65809786928df
Ura427 <urakost427@gmail.com>
2024-02-05 11:36:33 +0200

    add sorting options

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,25 @@ import TodoList from "./components/TodoList";
 import AddTodo from "./components/AddTodo";
 
 function App() {
-  const [lastId, setLastId] = useState<number>();
   const [todos, setTodos] = useState<ITodo[]>([
     {
       id: 1,
       task: "Learn how to work with interfaces",
-      completed: false,
       created: new Date().toDateString(),
     },
     {
       id: 2,
       task: "Fix the fridge",
-      completed: false,
       created: new Date().toDateString(),
     },
   ]);
+  const [sortOption, setSortOption] = useState<string>("id");
 
+  const sortOptionHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortOption(e.target.value);
+  };
+
+  console.log(sortOption);
   return (
     <div
       style={{
@@ -35,7 +38,17 @@ function App() {
       <h1>Todo App</h1>
       <AddTodo setTodos={setTodos}></AddTodo>
       <div style={{ margin: "2rem" }}>
-        <TodoList todos={todos} setTodos={setTodos}></TodoList>
+        <label htmlFor="sort">Sort by</label>
+        <select name="sort" onChange={sortOptionHandler}>
+          <option value={"id"}>id</option>
+          <option value={"alphabet"}>alphabet</option>
+          <option value={"date"}>date</option>
+        </select>
+        <TodoList
+          todos={todos}
+          setTodos={setTodos}
+          sortOption={sortOption}
+        ></TodoList>
       </div>
     </div>
   );
