6e3a56fcda155673f4c0bc07e58d940fa47d9f5f
Ura427 <urakost427@gmail.com>
2024-02-13 16:52:04 +0200

    now works directly with db

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,57 @@ import { ITodo } from "./types/types";
 import TodoList from "./components/TodoList";
 import AddTodo from "./components/AddTodo";
 
+import axios from "axios";
+
 function App() {
-  const [todos, setTodos] = useState<ITodo[]>([
-    {
-      id: 1,
-      task: "Learn how to work with interfaces",
-      created: new Date().toDateString(),
-    },
-    {
-      id: 2,
-      task: "Fix the fridge",
-      created: new Date().toDateString(),
-    },
-  ]);
-  const [sortOption, setSortOption] = useState<string>("id");
+  const [todos, setTodos] = useState<ITodo[]>([]);
+  // const [sortOption, setSortOption] = useState<string>("id");
 
-  const sortOptionHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSortOption(e.target.value);
+  // const sortOptionHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  //   setSortOption(e.target.value);
+  // };
+
+  // console.log(sortOption);
+
+
+
+
+  useEffect(() => {
+    fetchTodos()
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/api/todos");
+      if (Array.isArray(response.data)) {
+        const responseData: ITodo[] = response.data;
+        setTodos(responseData);
+      } else {
+        console.error("Invalid data format received:", response.data);
+      }
+    } catch (error) {
+      console.error("Error fetching todos:", error);
+    }
   };
+  
+/*   const fetchTodos = () => {
+    console.log("fetchTodos");
+    axios
+      .get("http://localhost:5000/api/todos")
+      .then((response) => {
+        if (Array.isArray(response.data)) {
+          const responseData: ITodo[] = response.data; // Ensure data is an array of ITodo
+          setTodos(responseData); // Update state
+        } else {
+          console.error("Invalid data format received:", response.data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching todos:", error);
+      });
+  } */
 
-  console.log(sortOption);
+
   return (
     <div
       style={{
@@ -36,18 +67,19 @@ function App() {
       }}
     >
       <h1>Todo App</h1>
-      <AddTodo setTodos={setTodos}></AddTodo>
+      <AddTodo fetchTodos={fetchTodos}></AddTodo>
       <div style={{ margin: "2rem" }}>
-        <label htmlFor="sort">Sort by</label>
+        {/* <label htmlFor="sort">Sort by</label>
         <select name="sort" onChange={sortOptionHandler}>
           <option value={"id"}>id</option>
           <option value={"alphabet"}>alphabet</option>
           <option value={"date"}>date</option>
-        </select>
+        </select> */}
         <TodoList
           todos={todos}
           setTodos={setTodos}
-          sortOption={sortOption}
+          fetchTodos={fetchTodos}
+          // sortOption={sortOption}
         ></TodoList>
       </div>
     </div>
