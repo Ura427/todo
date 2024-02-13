10d60cad65eca125f256655d3593b2e89cf702cc
Ura427 <urakost427@gmail.com>
2024-02-13 16:52:17 +0200

    now works directly with db

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,47 +1,31 @@
 import React, { useState } from "react";
 import { ITodo } from "../types/types";
+import axios from "axios";
 
 interface AddTodoProps {
-  setTodos: React.Dispatch<React.SetStateAction<ITodo[]>>;
+  // fetchTodos: Promise<void>;
+  fetchTodos: any
 }
 
-const AddTodo: React.FC<AddTodoProps> = ({ setTodos }) => {
-  const [inputValue, setInputValue] = useState<string>();
-  const [lastId, setLastId] = useState<number>(3);
+const AddTodo: React.FC<AddTodoProps> = ({ fetchTodos }) => {
+  const [inputValue, setInputValue] = useState<string>("");
+
 
   const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   };
 
   const btnClickHandler = (e: React.MouseEvent) => {
-    // Create a new Date object from the date string
-    const date = new Date();
-
-    // Get the month, date, year, hours, minutes, and seconds from the Date object
-    const month = date.toLocaleString("en", { month: "short" });
-    const day = date.getDate();
-    const year = date.getFullYear();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-
-    // Format the date in the desired format
-    const formattedDate = `${month} ${day} ${year} ` +
-  `${hours}:${minutes}:${seconds}`.replace(/(?<!\d)(\d)(?!\d)/g, '0$1');
-
-
-    //Create new Todo obj
-    const obj: ITodo = {
-      id: lastId,
-      task: inputValue,
-      completed: false,
-      created: formattedDate,
-    };
-
-    setTodos((prevState: ITodo[]) => [...prevState, obj]);
-    setInputValue("");
-    setLastId((prev) => prev + 1);
-    console.log("BTN clicked");
+   
+    axios
+      .post(`http://localhost:5000/api/todos/`, { task: inputValue })
+      .then((response) => {
+        fetchTodos()
+        console.log(response.data)
+      })
+      .catch((error: any) => {
+        console.error(error)
+      });
   };
 
   return (
