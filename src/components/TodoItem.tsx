d8ffb57f76281f5acd061224a2ef3fb285f5a526
Ura427 <urakost427@gmail.com>
2024-03-20 19:45:59 +0200

    add some styles for components

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -16,7 +16,12 @@ interface TodoItemProps {
   fetchTodos: () => Promise<void>;
 }
 
-const TodoItem: React.FC<TodoItemProps> = ({ todo, fetchTodos }) => {
+const TodoItem: React.FC<TodoItemProps> = ({
+  todo,
+  todos,
+  setTodos,
+  fetchTodos,
+}) => {
   const [disabled, setDisabled] = useState<boolean>(true);
   const [currTodo, setCurrTodo] = useState<ITodo>(todo);
 
@@ -26,7 +31,7 @@ const TodoItem: React.FC<TodoItemProps> = ({ todo, fetchTodos }) => {
       .delete(`http://localhost:5000/api/todos/${id}`)
       .then((response) => {
         console.log(response.data);
-        fetchTodos();
+        setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
       })
       .catch((error) => {
         console.error(`Error deleting element: ${error}`);
@@ -64,36 +69,91 @@ const TodoItem: React.FC<TodoItemProps> = ({ todo, fetchTodos }) => {
   return (
     <div
       style={{
-        background: "lightgreen",
-        border: "1px solid lightyellow",
+        // background: "#E3CAA5",
+        // background: "#AD8B73",
+        background: "#FFFBE9",
+        // background: "#CEAB93",
+        // border: "1px solid lightyellow",
+        borderRadius: "0.25rem",
         padding: "0.5rem",
+        margin: "0.25rem",
+        display: "flex",
+        alignItems: "center",
       }}
     >
-      {todo.id}:{" "}
+      <input
+        type="checkbox"
+        style={{
+          border: "none",
+        }}
+        onChange={() => handleCheckboxChange(todo.id)}
+      />
+      {/* <span>{todo.id}: </span> */}
       {disabled === true ? (
-        <>{currTodo.task}</>
+        <span
+          style={{
+            color: "#AD8B73",
+            // color: "#CEAB93"
+            // color: "#FFFBE9"
+            // color: "#FFF"
+            flex: "1",
+            fontFamily: "Bad Script",
+            fontWeight: "400",
+            fontStyle: "normal",
+            margin: "0 0.5rem"
+
+          }}
+        >
+          {currTodo.task}
+        </span>
       ) : (
         <>
           <input
             type="text"
             value={currTodo.task}
+            style={{
+              flex: "1",
+              border: "none",
+              outline: "none",
+            }}
             onChange={handleTaskChange}
           ></input>{" "}
         </>
       )}
-      <input type="checkbox" onChange={() => handleCheckboxChange(todo.id)} />
-      <button onClick={() => btnClickHandler(todo.id)}>
+      <button
+        style={{
+          border: "none",
+          // backgroundColor: "#CEAB93"
+          backgroundColor: "#E3CAA5",
+          borderRadius: "1rem"
+        }}
+        onClick={() => btnClickHandler(todo.id)}
+      >
         {disabled === true ? (
-          <>
-            <EditIcon /> Edit
-          </>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            {/* <span>Edit</span>  */}
+            <EditIcon />
+          </div>
         ) : (
-          <>
-            <DoneIcon></DoneIcon> Done
-          </>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            {/* <span>Done</span>  */}
+            <DoneIcon />
+          </div>
         )}
-      </button>{" "}
-      {todo.created.toString()}
+      </button>
+      <span
+        style={{
+          fontSize: "0.5rem",
+          marginTop: "1rem",
+          marginLeft: "0.5rem",
+          whiteSpace: "nowrap",
+          color: "#AD8B73",
+          fontWeight: "bold",
+          fontFamily: "Open Sans"
+        }}
+      >
+        {todo.created.toString()}
+      </span>
     </div>
   );
 };
