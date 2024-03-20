cf041b04f95c4447b6e7cb2b291bf98c29d11568
Ura427 <urakost427@gmail.com>
2024-03-20 23:02:53 +0200

    use InifiniteScroll component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,25 +1,47 @@
 import React from "react";
 
-
 //TYPES
 import { ITodo } from "../types/types";
 
 //COMPONENTS
 import TodoItem from "./TodoItem";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 //PROPS
 interface TodoListProps {
   todos: ITodo[];
   setTodos: React.Dispatch<React.SetStateAction<ITodo[]>>;
-  fetchTodos: () => Promise<void>
+  fetchTodos: () => Promise<void>;
+  hasMore: boolean
 }
 
-const TodoList: React.FC<TodoListProps> = ({ todos, setTodos, fetchTodos }) => {
+const TodoList: React.FC<TodoListProps> = ({ todos, setTodos, fetchTodos, hasMore }) => {
   return (
     <div>
-      {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} todos={todos} setTodos={setTodos} fetchTodos={fetchTodos}/>
-      ))}
+       <InfiniteScroll
+          dataLength={todos.length} //This is important field to render the next data
+          next={fetchTodos}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center", color: "#FFF", fontFamily: "Open Sans" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+          style={{
+            width: "500px"
+          }}
+        >
+          {todos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              todos={todos}
+              setTodos={setTodos}
+              fetchTodos={fetchTodos}
+            />
+          ))}
+        </InfiniteScroll>
     </div>
   );
 };
